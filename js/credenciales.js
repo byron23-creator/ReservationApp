88acae2f4ffea5f3a46be2eb993d554ab40ba10e
Byron Fernando Cardona Sánchez <68670367+byron23-creator@users.noreply.github.com>
2025-07-08 17:39:07 -0600

    Delete js directory

--- js/credenciales.js
+++
@@ -1,3 +0,0 @@
-const CLIENT_ID = '668538191270-v5d9om5a3pvs3qnotsnnlpk0dt59hohp.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyD89RHM_kUqfSqILL2Z7uTF5cr13xL1s30';
-const SPREADSHEET = "1XJmPp9hwURfuIx4-mNIyIY37iuWP8uZTP_HtDKHVzN4";
